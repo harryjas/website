42d51dc8aa7e794987e90098e2730087e95a3989
harryjas <harryjasrotia@ymail.com>
2017-10-02 14:17:26 +0530

    Revert "settings.py: Add directories for templates and staticfiles"
    This reverts commit 0077dd23711fc02aed469d8c9e8e0abc1f556df2.

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
-    template_name = 'home/home.html'
+    template_name = 'home.html'
 
 class Resources(TemplateView):
-    template_name = 'resources/resources.html'
+    template_name = 'resources.html'
